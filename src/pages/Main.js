21a2ec691efb509889f8e9dfb62990972a8eca06
Marco Aurélio <marcooli.pereira@gmail.com>
2020-03-05 13:26:49 -0300

    Inclui botao de favoritos

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -6,14 +6,19 @@ import {
     Image,
     FlatList,
     Button,
+    TouchableHighlight,
 } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { setFavorite, getFavorite } from '../FavoriteAction';
+
 
 export default class Main extends Component {
 
     constructor(props){
         super(props);
         this.state = {
-            data: []
+            data: [],
+            favoritos: []
         }
     }
 
@@ -27,27 +32,55 @@ export default class Main extends Component {
         })
     }
 
+    escolherFavorito = async (item) => {
+        this.setState({
+            favoritos: await setFavorite(item)
+        })
+    }
+
+    loadFavorites = async () => {
+        this.setState({
+          favoritos: await getFavorite()  
+        })
+    }
+
+     isfavorite = (item) => {
+        return this.state.favoritos.filter(f=> f.id == item.id)[0]
+     }
+
     componentDidMount() {
         this.loadUsers();
+        this.loadFavorites();
     }
 
     render() {
         return (
-            <View style={styles.containerCard}>
-                <FlatList 
-                    data={this.state.data}
-                    renderItem={({item}) => (
-                        <View style={styles.containerCard}>
-                        <Text style={styles.title}>{item.title}</Text>
-                        <Image 
-                        source={{ uri: item.image_url }}
-                        style={styles.image} 
-                        />
-                        <Button style={styles.button} title='Ver Mais!' onPress={() => {this.props.navigation.navigate("Details", {noticia: item} )}}></Button>
-                    </View> 
-                    )}
-                />
-                
+            <View style={styles.container}>
+                <View style={styles.containerCard}>
+                    <FlatList 
+                        data={this.state.data}
+                        renderItem={({item}) => (
+                            <View style={styles.containerCard}>
+                            <Text style={styles.title}>{item.title}</Text>
+                            <Image 
+                            source={{ uri: item.image_url }}
+                            style={styles.image} 
+                            />
+                            <View style={styles.buttonContainer}>
+                                <Button 
+                                title='Ver Mais!' 
+                                onPress={() => {this.props.navigation.navigate("Details", {noticia: item} )}}
+                                color="#489A12">
+                                </Button>
+                                <TouchableHighlight onPress={()=> this.escolherFavorito(item)}>
+                                <Icon name="md-star" size={30} color={this.isfavorite(item) ? 'yellow' : 'gray'}/>
+                                </TouchableHighlight>
+                            </View>
+                        </View> 
+                        )}
+                    />
+                    
+                </View>
             </View>
         );
     }
@@ -56,24 +89,38 @@ export default class Main extends Component {
 
 
 const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#ccc',
+    },
     containerCard: {
         flex: 1,
-        margin: 3,
-        padding: 3,
+        margin: 5,
+        padding: 5,
         alignItems: 'center',
         justifyContent: 'space-around',
-        height: 530,
+        height: 300,
+        backgroundColor: '#fff',
     },
     title: {
         fontSize: 20,
         fontWeight: 'bold',
         marginBottom: 10,
-        textAlign: 'center'
+        textAlign: 'center',
+        color: '#489A12'
     },
     image: {
         width: 300, 
-        height: 280,
+        height: 150,
         alignItems: 'center',
         justifyContent: 'center',
     },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        width: 200
+    },
+    favText: {
+        fontSize: 16,
+        fontWeight: 'bold',
+    }
 })
